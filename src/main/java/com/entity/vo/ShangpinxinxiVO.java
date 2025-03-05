b624dd4d1e46e19e6ae96ee44f7f84b67565830a
yulinyu <2508000341@QQ.COM>
2025-03-05 16:02:20 +0800

    1

---
+++ src/main/java/com/entity/vo/ShangpinxinxiVO.java
@@ -0,0 +1,271 @@
+package com.entity.vo;
+
+import com.entity.ShangpinxinxiEntity;
+
+import com.baomidou.mybatisplus.annotations.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.Date;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+ 
+
+/**
+ * 商品信息
+ * 手机端接口返回实体辅助类 
+ * （主要作用去除一些不必要的字段）
+ * @author 
+ * @email 
+ * @date 2022-03-20 11:39:50
+ */
+public class ShangpinxinxiVO  implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	 			
+	/**
+	 * 商品名称
+	 */
+	
+	private String shangpinmingcheng;
+		
+	/**
+	 * 商品类型
+	 */
+	
+	private String shangpinleixing;
+		
+	/**
+	 * 图片
+	 */
+	
+	private String tupian;
+		
+	/**
+	 * 品牌
+	 */
+	
+	private String pinpai;
+		
+	/**
+	 * 保修期
+	 */
+	
+	private String baoxiuqi;
+		
+	/**
+	 * 上架日期
+	 */
+		
+	@JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
+	@DateTimeFormat 
+	private Date shangjiariqi;
+		
+	/**
+	 * 器材介绍
+	 */
+	
+	private String qicaijieshao;
+		
+	/**
+	 * 最近点击时间
+	 */
+		
+	@JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
+	@DateTimeFormat 
+	private Date clicktime;
+		
+	/**
+	 * 价格
+	 */
+	
+	private Float price;
+		
+	/**
+	 * 单限
+	 */
+	
+	private Integer onelimittimes;
+		
+	/**
+	 * 库存
+	 */
+	
+	private Integer alllimittimes;
+				
+	
+	/**
+	 * 设置：商品名称
+	 */
+	 
+	public void setShangpinmingcheng(String shangpinmingcheng) {
+		this.shangpinmingcheng = shangpinmingcheng;
+	}
+	
+	/**
+	 * 获取：商品名称
+	 */
+	public String getShangpinmingcheng() {
+		return shangpinmingcheng;
+	}
+				
+	
+	/**
+	 * 设置：商品类型
+	 */
+	 
+	public void setShangpinleixing(String shangpinleixing) {
+		this.shangpinleixing = shangpinleixing;
+	}
+	
+	/**
+	 * 获取：商品类型
+	 */
+	public String getShangpinleixing() {
+		return shangpinleixing;
+	}
+				
+	
+	/**
+	 * 设置：图片
+	 */
+	 
+	public void setTupian(String tupian) {
+		this.tupian = tupian;
+	}
+	
+	/**
+	 * 获取：图片
+	 */
+	public String getTupian() {
+		return tupian;
+	}
+				
+	
+	/**
+	 * 设置：品牌
+	 */
+	 
+	public void setPinpai(String pinpai) {
+		this.pinpai = pinpai;
+	}
+	
+	/**
+	 * 获取：品牌
+	 */
+	public String getPinpai() {
+		return pinpai;
+	}
+				
+	
+	/**
+	 * 设置：保修期
+	 */
+	 
+	public void setBaoxiuqi(String baoxiuqi) {
+		this.baoxiuqi = baoxiuqi;
+	}
+	
+	/**
+	 * 获取：保修期
+	 */
+	public String getBaoxiuqi() {
+		return baoxiuqi;
+	}
+				
+	
+	/**
+	 * 设置：上架日期
+	 */
+	 
+	public void setShangjiariqi(Date shangjiariqi) {
+		this.shangjiariqi = shangjiariqi;
+	}
+	
+	/**
+	 * 获取：上架日期
+	 */
+	public Date getShangjiariqi() {
+		return shangjiariqi;
+	}
+				
+	
+	/**
+	 * 设置：器材介绍
+	 */
+	 
+	public void setQicaijieshao(String qicaijieshao) {
+		this.qicaijieshao = qicaijieshao;
+	}
+	
+	/**
+	 * 获取：器材介绍
+	 */
+	public String getQicaijieshao() {
+		return qicaijieshao;
+	}
+				
+	
+	/**
+	 * 设置：最近点击时间
+	 */
+	 
+	public void setClicktime(Date clicktime) {
+		this.clicktime = clicktime;
+	}
+	
+	/**
+	 * 获取：最近点击时间
+	 */
+	public Date getClicktime() {
+		return clicktime;
+	}
+				
+	
+	/**
+	 * 设置：价格
+	 */
+	 
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	
+	/**
+	 * 获取：价格
+	 */
+	public Float getPrice() {
+		return price;
+	}
+				
+	
+	/**
+	 * 设置：单限
+	 */
+	 
+	public void setOnelimittimes(Integer onelimittimes) {
+		this.onelimittimes = onelimittimes;
+	}
+	
+	/**
+	 * 获取：单限
+	 */
+	public Integer getOnelimittimes() {
+		return onelimittimes;
+	}
+				
+	
+	/**
+	 * 设置：库存
+	 */
+	 
+	public void setAlllimittimes(Integer alllimittimes) {
+		this.alllimittimes = alllimittimes;
+	}
+	
+	/**
+	 * 获取：库存
+	 */
+	public Integer getAlllimittimes() {
+		return alllimittimes;
+	}
+			
+}
